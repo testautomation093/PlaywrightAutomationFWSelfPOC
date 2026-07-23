d71b2091592c16a20ce116cb10cb86456151c4ad
“Nishant <testautomation093@gmail.com>
2026-07-23 17:54:53 +0530

    Updated project with yml file and make some changes in the pw config file as well

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    headless: false
+    headless: process.env.CI ? true : false
   },
 
   /* Configure projects for major browsers */
