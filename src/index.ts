c4c74f59ed4f396aeb2c47325b2a6be17d7fbf2d
“Nishant <testautomation093@gmail.com>
2026-07-27 13:32:02 +0530

    Addedd index.ts and .npmignore file for the publishment of package

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// exporting pages
+
+export {BasePage} from './pages/BasePage.js';
+export {HomePage} from './pages/HomePage.js';
+export {SearchResultsPage} from './pages/SearchResultsPage.js';
+export {ProductInfoPage} from './pages/ProductInfoPage.js';
+export {LoginPage} from './pages/LoginPage.js';
+
+// exporting fixtures :
+
+export { test, expect } from "./fixtures/pageFixtures.js";
+
+// API Export :
+
+export {APIHelper} from './api/APIHelper.js';
+
+// Utils Export :
+
+export {CsvHelper} from './utils/csvUtils.js';
+export {JsonHelper} from './utils/jsonUtils.js';
+export {ExcelHelper} from './utils/excelUtils.js';
